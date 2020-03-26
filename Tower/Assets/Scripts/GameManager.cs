462164d423b74285001aa71c04504cec92637814
Camille <camille.lavarde@gmail.com>
2020-03-26 23:24:23 +0100

    setup rewired

---
+++ Tower/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    public Setting setting = new Setting();
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+public class Setting
+{
+    public float playersSpeed = 1f;
+}
---
+++ Tower/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int playerId = 0;
+    [SerializeField] private Player player;
+
+    private float speed;
+
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+
+        speed = GameManager.instance.setting.playersSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = player.GetAxis("MoveX");
+        float moveVertical = player.GetAxis("MoveY");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        transform.position += movement * speed * Time.fixedDeltaTime;
+
+        if (player.GetButtonDown("Interact"))
+        {
+            Debug.Log("Player "+ playerId + " : Interact");
+        }
+        if (player.GetButtonDown("Throw"))
+        {
+            Debug.Log("Player " + playerId + " : Throw");
+        }
+    }
+}
