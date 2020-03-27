9cd679900aeda0d1f88dfb0e7045884c7242d68e
Camille <camille.lavarde@gmail.com>
2020-03-27 12:46:56 +0100

    orientation player

--- Tower/Assets/Scripts/PlayerController.cs
+++ Tower/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,9 @@ public class PlayerController : MonoBehaviour
 
     private float speed;
 
+    // debug
+    float orientY;
+
     private void Start()
     {
         player = ReInput.players.GetPlayer(playerId);
@@ -19,11 +22,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float moveHorizontal = player.GetAxis("MoveX");
-        float moveVertical = player.GetAxis("MoveY");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
-        transform.position += movement * speed * Time.fixedDeltaTime;
+        Rotate(Move());
 
         if (player.GetButtonDown("Interact"))
         {
@@ -33,5 +32,43 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Player " + playerId + " : Throw");
         }
+    }
+
+    private Vector3 Move()
+    {
+        float moveHorizontal = player.GetAxis("MoveX");
+        float moveVertical = player.GetAxis("MoveY");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        transform.position += movement * speed * Time.fixedDeltaTime;
+        return movement;
+    }
+
+    private void Rotate(Vector3 movement)
+    {
+        float moveHorizontal = player.GetAxis("OrientX");
+        float moveVertical = player.GetAxis("OrientY");
+
+        if (moveVertical == 0 && moveHorizontal == 0)
+        {
+            if (movement.z == 0 && movement.x == 0)
+            {
+                return;
+            }
+            orientY = Mathf.Atan2(movement.z, movement.x) * Mathf.Rad2Deg + 90;
+        }
+        else
+        {
+            orientY = Mathf.Atan2(moveVertical, moveHorizontal) * Mathf.Rad2Deg + 90;
+        }
+
+        Vector3 orient = new Vector3(0, -orientY, 0);
+        transform.localEulerAngles = orient;
+    }
+
+    void OnGUI()
+    {
+        //Output the angle found above
+        GUI.Label(new Rect(25, 25, 200, 40), "Angle Between Objects : " + orientY);
     }
 }
